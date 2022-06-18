f54ef54a68710cd1532a545d40c090bbcef90a5a
Evgen1ya <srtik93@gmail.com>
2022-06-18 18:14:13 +0700

    Курсовая работа 2.0

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
+import json
 from pprint import pprint
+import os
+import logging
+from datetime import date
+import yadisk
+
 
 URL = 'https://api.vk.com/method/photos.get'
-TOKEN = 'token vk'
+TOKEN = 'd'
 count = 5
 params = {
     'user_ids': '8175842',
@@ -17,22 +23,17 @@ params = {
 res = requests.get(URL, params=params).json()
 # pprint(res)
 
-
-# name = res['response']['items'][0]['likes']['count']
 item = res['response']['items']
+# pprint(item)
 
 for el in item:
-    # pprint(type(el))
-    for picture in el.get('likes'):
-        # print(type(picture)) Почему выводится строка, если у ключа likes должен быть словарь?
-        # name = picture.get('count')
-        pprint(picture)
-
-for el in item:
+    photo = {}
+    file_name = el['likes']['count']
     for picture in el.get('sizes'):
         if picture.get('type') == 'z':
             photo_url = picture.get('url')
-            pprint(photo_url)
+            photo[file_name] = photo_url
+    # pprint(photo)
 
 class YaUploader:
     def __init__(self, token: str):
@@ -47,20 +48,29 @@ class YaUploader:
     def _get_upload_link(self, disk_file_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-        params = {"path": disk_file_path, "overwrite": "true", "url": photo_url}
+        params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
         pprint(response.json())
         return response.json()
 
     def upload_file_to_disk(self, disk_file_path, filename):
         href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
-        response = requests.post(href, data=open(filename, 'rb'))
+        response = requests.put(href, data=open(filename, 'rb'))
         response.raise_for_status()
         if response.status_code == 201:
             print("Success")
 
 if __name__ == '__main__':
-    path_to_file = f"photo/{name}"
-    token = 'token YD'
+    token = ''
     uploader = YaUploader(token)
-    result = uploader.upload_file_to_disk(path_to_file, photo_url)
+    for key, value in photo.items():
+        file_path = f"photo/{key}"
+        result = uploader.upload_file_to_disk(file_path, value)
+
+# В чем здесь ошибка может быть? Код беру из предыдущего домашнего задания. Он точно работал, потому что файл с ноутбука загружался на ЯД,
+# но если менять на URL фото из VK, то код не работает.
+
+y = yadisk.YaDisk(token='')
+y.upload(file_name, photo_url)
+
+# Хотела попробовать через библиотеку, но также не получается по URL

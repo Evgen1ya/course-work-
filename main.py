17c9af912b68813e5b78cf8eb68f43bbab80fcd9
Evgen1ya <srtik93@gmail.com>
2022-06-18 23:58:54 +0700

    Курсовая работа

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import yadisk
 
 
 URL = 'https://api.vk.com/method/photos.get'
-TOKEN = 'd'
+TOKEN = ''
 count = 5
 params = {
     'user_ids': '8175842',
@@ -48,14 +48,14 @@ class YaUploader:
     def _get_upload_link(self, disk_file_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-        params = {"path": disk_file_path, "overwrite": "true"}
+        params = {"path": disk_file_path, "overwrite": "true", "url": photo_url}
         response = requests.get(upload_url, headers=headers, params=params)
         pprint(response.json())
         return response.json()
 
-    def upload_file_to_disk(self, disk_file_path, filename):
+    def upload_file_to_disk(self, disk_file_path, url):
         href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
-        response = requests.put(href, data=open(filename, 'rb'))
+        response = requests.put(href, url)
         response.raise_for_status()
         if response.status_code == 201:
             print("Success")
@@ -63,14 +63,8 @@ class YaUploader:
 if __name__ == '__main__':
     token = ''
     uploader = YaUploader(token)
-    for key, value in photo.items():
-        file_path = f"photo/{key}"
-        result = uploader.upload_file_to_disk(file_path, value)
+for key, value in photo.items():
+    file_path = f"photo/{key}.png"
+result = uploader.upload_file_to_disk(file_path, value)
 
-# В чем здесь ошибка может быть? Код беру из предыдущего домашнего задания. Он точно работал, потому что файл с ноутбука загружался на ЯД,
-# но если менять на URL фото из VK, то код не работает.
 
-y = yadisk.YaDisk(token='')
-y.upload(file_name, photo_url)
-
-# Хотела попробовать через библиотеку, но также не получается по URL
